15524b2b6823cd402aefb26658ffdc48dadb1918
Rone Silveira <ronebs10@hotmail.com>
2020-08-04 17:10:57 -0300

    created first route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.get("/users", (request, response) => {
+    const users = [
+        { name: 'Rone', age: 33 },
+        { name: 'Fulano', age: 30 },
+    ]
+
+    return response.json([users]);
+});
+
+// localhost:3333/users
+
+app.listen(3333);
